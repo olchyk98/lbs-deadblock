56190c5cb8eb20650843145275a4188c2884da9d
Oles Odynets <olchyk98@gmail.com>
2021-07-05 14:55:19 +0300

    feat(logic): implement dynamic blocks

---
+++ Classes/Logic/DynamicBlock.cs
@@ -0,0 +1,31 @@
+using Deadblock.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Deadblock.Logic
+{
+    public abstract class DynamicBlock : DeliveredGameSlot, ISpriteBlock
+    {
+        // NOTE: Class implements ISpriteBlock using
+        // a native instance of SpriteBlock.
+        // This is kind of solution is designed
+        // to bring more freedom for extending and supporting this class.
+        public SpriteBlock MainSprite { get; private set; }
+
+        public DynamicBlock(GameProcess aGame, string aTextureKey) : base(aGame)
+        {
+            MainSprite = new SpriteBlock(aGame, aTextureKey);
+        }
+
+        virtual public void Render(Vector2 aPosition, bool isRelative = true) {
+            MainSprite.Render(aPosition, isRelative);
+            Update();
+        }
+
+        virtual public void Update() { }
+
+        virtual public Vector2 GetDimensions()
+        {
+            return MainSprite.GetDimensions();
+        }
+    }
+}
---
+++ Classes/Logic/TreeSpawner.cs
@@ -0,0 +1,71 @@
+using Deadblock.Engine;
+using Deadblock.Generic;
+using Deadblock.Tools;
+using Microsoft.Xna.Framework;
+
+namespace Deadblock.Logic
+{
+    public class TreeSpawner : InteractableDynamicBlock
+    {
+        private float mySpawnQuota;
+        private bool myHasPlant;
+        private SpriteBlock myPlantSprite;
+
+        public TreeSpawner(GameProcess aGame, string aMainTextureKey) : base(aGame, aMainTextureKey)
+        {
+            mySpawnQuota = NativeUtils.RandomizeValue(10f, 20f);
+            myPlantSprite = new SpriteBlock(gameInstance, "env/green-tree");
+        }
+
+        override public void Render(Vector2 aPosition, bool isRelative = true)
+        {
+
+            base.Render(aPosition, false);
+
+            if (myHasPlant)
+            {
+                myPlantSprite.Render(aPosition, true);
+            }
+        }
+
+        override public void Update()
+        {
+            base.Update();
+
+            if (mySpawnQuota > 0)
+            {
+                mySpawnQuota -= .1f;
+            }
+
+            if(mySpawnQuota <= 0 && !myHasPlant)
+            {
+                myHasPlant = true;
+            }
+        }
+
+        override public void InteractWith(Entity anEntity)
+        {
+            Chop();
+
+            base.InteractWith(anEntity);
+        }
+
+        /// <summary>
+        /// Chops the tree and
+        /// resets the quota value.
+        /// </summary>
+        /// <returns>
+        /// Boolean that represents
+        /// if the tree has been chopped.
+        /// </returns>
+        private bool Chop()
+        {
+            if (!myHasPlant) return false;
+
+            mySpawnQuota = NativeUtils.RandomizeValue(5f, 15f);
+            myHasPlant = false;
+
+            return true;
+        }
+    }
+}
